a1ed6124ed7091c4847a0bc7534fafe23fb8f3f4
hoang dzung <dungmin97@gmail.com>
2019-05-10 04:18:04 +0700

    config output layer

--- src/main.py
+++ src/main.py
@@ -10,8 +10,21 @@ def main():
     tab_printer(args)
     if args.zout:
         graph = girvan_graphs(args.zout)
+        args.layers = args.layers + [4]
     elif args.mu:
         graph = LFR(args.zout)
+        node2com=dict()
+        n_com = 0
+        for node in self.graph.nodes():
+            is_old = False
+            for i in self.graph.nodes[node]['community']:
+                if i in node2com:
+                    is_old = True
+                    break
+                node2com[i]=n_com
+            if not is_old:
+                n_com+=1
+        args.layers = args.layers + [n_com]
     else:
         raise NotImplementedError
     # graph = read_graph(args)
